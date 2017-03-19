c1fcb19b290325aa6de10d3f76ba68a564541cbd
avnestico <avnestico@users.noreply.github.com>
2017-03-19 17:15:57 -0400

    Added auto scaling page

--- manager/admin.py
+++ manager/admin.py
@@ -1,10 +1,65 @@
+from flask import request
+
 from manager import app
 
 from flask import render_template, redirect, url_for
 
+from manager import monitor_pool
 
-@app.route('/admin')
+
+@app.route('/admin', methods=['GET'])
 # main landing page
 def admin():
     return render_template("/admin/admin.html",
-                           page_header="Welcome to ECE1771 Assignment 1 Manager")
+                           page_header="Welcome to ECE1771 Assignment 1 Manager",
+                           pv=monitor_pool.PolicyVars())
+
+
+@app.route('/admin/update', methods=['POST'])
+def admin_update():
+
+    try:
+        high = int(request.form.get('high'))
+        low = int(request.form.get('low'))
+        mult = int(request.form.get('mult'))
+        div = int(request.form.get('div'))
+    except:
+        errors = ["All inputs must be numeric"]
+        return render_template("/admin/admin.html",
+                               page_header="Welcome to ECE1771 Assignment 1 Manager",
+                               pv=monitor_pool.PolicyVars(),
+                               errors=errors)
+
+    errors = check_errors(high, low, mult, div)
+    if errors:
+        return render_template("/admin/admin.html",
+                               page_header="Welcome to ECE1771 Assignment 1 Manager",
+                               pv=monitor_pool.PolicyVars(),
+                               errors=errors)
+
+    pv = monitor_pool.PolicyVars()
+    pv.high_cpu_threshold = high
+    pv.low_cpu_threshold = low
+    pv.scaling_multiplier = mult
+    pv.scaling_divisor = div
+
+    return render_template("/admin/admin.html",
+                           page_header="Welcome to ECE1771 Assignment 1 Manager",
+                           pv=monitor_pool.PolicyVars())
+
+
+def check_errors(high, low, mult, div):
+    errors = []
+
+    if high > 100:
+        errors.append("High CPU threshold cannot be larger than 100")
+    if high <= low:
+        errors.append("High CPU threshold must be larger than low CPU threshold")
+    if low < 0:
+        errors.append("High CPU threshold cannot be less than 0")
+    if mult < 2:
+        errors.append("Scaling multiplier must be 2 or larger")
+    if div < 2:
+        errors.append("Scaling divisor must be 2 or larger")
+
+    return errors
--- manager/monitor_pool.py
+++ manager/monitor_pool.py
@@ -4,8 +4,16 @@ import statistics
 
 from manager import workers, db, loadbalancer
 
-high_cpu_threshold = 40
-low_cpu_threshold = 20
+class PolicyVars(object):
+    __instance = None
+    def __new__(cls):
+        if PolicyVars.__instance is None:
+            PolicyVars.__instance = object.__new__(cls)
+            PolicyVars.__instance.high_cpu_threshold = 40
+            PolicyVars.__instance.low_cpu_threshold = 20
+            PolicyVars.__instance.scaling_multiplier = 2
+            PolicyVars.__instance.scaling_divisor = 2
+        return PolicyVars.__instance
 
 
 def background_monitor():
@@ -24,6 +32,9 @@ def background_monitor():
         # get all worker instances registered to the load balancer
         instances = loadbalancer.get_all_instances()
 
+        # get current policy variables
+        pv = PolicyVars()
+
         cpu_utilizations = []
         print('worker instances: ')
         for instance in instances:
@@ -37,16 +48,16 @@ def background_monitor():
             else:
                 running_workers += 1
 
-
             cpu = workers.get_worker_utilization(instance.id)
             max_cpu = 0
             for point in cpu['Datapoints']:
                 max_cpu = max(max_cpu, point['Maximum'])
             print(instance.id + ": max cpu: " + str(max_cpu))
             cpu_utilizations.append(max_cpu)
-            if max_cpu > high_cpu_threshold:
+
+            if max_cpu > pv.high_cpu_threshold:
                 busy_workers += 1
-            elif max_cpu < low_cpu_threshold:
+            elif max_cpu < pv.low_cpu_threshold:
                 quiet_workers += 1
 
         average_utilization = statistics.mean(cpu_utilizations)
@@ -65,10 +76,10 @@ def background_monitor():
             workers.create_ec2_worker(db.db_config['host'])
             time.sleep(30)
 
-        if average_utilization > high_cpu_threshold:
+        if average_utilization > pv.high_cpu_threshold:
             print('going to grow pool...')
             workers.grow_pool()
-        elif average_utilization < low_cpu_threshold:
+        elif average_utilization < pv.low_cpu_threshold:
             if running_workers == 1:
                 print('only one worker, not shrinking')
             else:
